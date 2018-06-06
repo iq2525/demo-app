f664b82fbc6a2683cc580a7abb65998e4e096972
i.ahmed <iqbal.ahmed@net-a-porter.com>
2018-06-06 10:59:21 +0100

    Deploy version commit

--- src/App.js
+++ src/App.js
@@ -5,27 +5,40 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const BasicExample = () => (
-  <Router>
+  <section>
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
+      <h2>Welcome to React</h2>
 
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <small>
+        You are running this application in <b>{process.env.NODE_ENV}</b> mode.
+      </small>
+      <form>
+        <input type="hidden" defaultValue={process.env.REACT_APP_SECRET_CODE} />
+      </form>
     </div>
-  </Router>
+
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+  </section>
 );
 
 const Home = () => (
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders welcome message", () => {
+  debugger;
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to React</h2>;
+  // expect(wrapper.contains(welcome)).to.equal(true);
+  expect(wrapper).toContainReact(welcome);
 });
